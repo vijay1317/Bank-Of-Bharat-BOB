2d1572ef65d9c11941bb47fb4d2f392b44f9fe1b
Manish <manishaarya029@gmail.com>
2024-06-03 23:17:44 +0530

    All code commit

---
+++ src/main/java/com/syc/finance/v1/bharat/constants/AccountDetailsConstants.java
@@ -0,0 +1,27 @@
+package com.syc.finance.v1.bharat.constants;
+import lombok.Data;
+
+@Data
+public class AccountDetailsConstants {
+
+    public static final String BANK_VI_NAME = "Bank of Bharat";
+    public static final String BANK_VI_BRANCH = "Bank of Bharat - Bhopal Indian Central Branch";
+    public static final String BANK_VI_ROUTING = "Bank of Bharat - Bhopal City Branch 456 Park Road MP Nagar Bhopal, 462011 India";
+    public static final String BANK_VI_STATUS = "002";
+    public static final String BANK_VI_ACCOUNT_CREATED = "YOUR ACCOUNT HAS BEEN CREATED SUCCESSFULLY !! ";
+    public static final String BANK_VI_ACCOUNT_BALANCE_STATUS = "Your Current Balance";
+    public static final String BANK_VI_ACCOUNT_BALANCE_CREDIT = "Money Has Been Credited Successfully !!";
+    public static final String BANK_VI_ACCOUNT_BALANCE_DEBITED = "Money Has Been Debited Successfully !!";
+    public static final String TWILIO_ACCOUNT_SID = "ACd43b45b461dafda49453b040eab3a27f";
+    public static final String TWILIO_AUTH_TOKEN = "0e5f6c2b2bf9508cf17e96f8d2170316";
+    public static final String TWILIO_PHONE_NUMBER = "+12313594081";
+    public static final String BANK_V2_CREDIT = "CREDIT";
+    public static final String BANK_V2_DEBIT = "DEBIT";
+    public static final String BANK_V3_NOTA_UPI_ID = "This account does not have a UPI ID.";
+    public static final String BANK_V3_UPI_CREATED = "Your UPI ID has been successfully created !!";
+    public static final String SUCCESS_PAY_MONEY_FROM_UPI = "Payment Successfully !!";
+    public static final String SUCCESS_STATUS = "SUCCESS !!";
+    public static final String SUCCESS_ADD_MONEY_TO_UPI_FROM_MAIN_ACCOUNT = "Your Money Hass been added to your upi account successfully !!";
+    public static final String MONEY_SEND_SUCCESSFULLY = "Money Has Been Send Successfully, Thank Your For Using Bank Of Bharat !!";
+
+}
